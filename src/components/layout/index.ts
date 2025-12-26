fb42fe21841b3bd0dd29ff19b9b9bf9a5c3212aa
supriya <supriyakambali67@gmail.com>
2025-12-26 23:38:45 +0530

    Clean up: Remove duplicate root files, keep only backend and frontend folders

--- src/components/layout/index.ts
+++
@@ -1,5 +0,0 @@
-export { Navbar } from './Navbar';
-export { Footer } from './Footer';
-export { DashboardSidebar } from './DashboardSidebar';
-export { DashboardLayout } from './DashboardLayout';
-export { PublicLayout } from './PublicLayout';
